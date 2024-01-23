aa800350bfb609cc6ad0a9d60945d16cd2c17f54
JoseSagwe <Josephsagwe421@gmail.com>
2024-01-23 09:24:24 +0300

    first commit

---
+++ src/main/java/com/mpesa_integration/MpesaIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.mpesa_integration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MpesaIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MpesaIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mpesa_integration/MpesaIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mpesa_integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MpesaIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
