6724ac486c40a065f15b546a30c06f58bd1c14de
JoseSagwe <Josephsagwe421@gmail.com>
2024-01-25 08:52:02 +0300

    okhttp bean in main class

--- src/main/java/com/mpesa_integration/MpesaIntegrationApplication.java
+++ src/main/java/com/mpesa_integration/MpesaIntegrationApplication.java
@@ -1,7 +1,10 @@
 package com.mpesa_integration;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import okhttp3.OkHttpClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MpesaIntegrationApplication {
@@ -9,5 +12,16 @@ public class MpesaIntegrationApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MpesaIntegrationApplication.class, args);
 	}
+
+	@Bean
+	public OkHttpClient getOkHttpClient() {
+		return new OkHttpClient();
+	}
+
+	@Bean
+	public ObjectMapper getObjectMapper() {
+		return new ObjectMapper();
+	}
+
 
 }
