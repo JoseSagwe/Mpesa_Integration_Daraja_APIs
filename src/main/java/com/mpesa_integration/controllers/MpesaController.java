27f575f10048293aaa73e515570a34af1ab83196
JoseSagwe <Josephsagwe421@gmail.com>
2024-02-03 18:58:33 +0300

    register url controller class

--- src/main/java/com/mpesa_integration/controllers/MpesaController.java
+++ src/main/java/com/mpesa_integration/controllers/MpesaController.java
@@ -9,18 +9,34 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("mpesa")
-
+@RequestMapping("mobile-money")
 public class MpesaController {
 
     private final DarajaApi darajaApi;
+    private final AcknowledgeResponse acknowledgeResponse;
 
-    public MpesaController(DarajaApi darajaApi) {
+    public MpesaController(DarajaApi darajaApi, AcknowledgeResponse acknowledgeResponse) {
         this.darajaApi = darajaApi;
+        this.acknowledgeResponse = acknowledgeResponse;
     }
 
     @GetMapping(path = "/token", produces = "application/json")
     public ResponseEntity<AccessTokenResponse> getAccessToken() {
         return ResponseEntity.ok(darajaApi.getAccessToken());
+    }
+
+    @GetMapping(path = "/register-url", produces = "application/json")
+    public ResponseEntity<RegisterUrlResponse> registerUrl() {
+        return ResponseEntity.ok(darajaApi.registerUrl());
+    }
+
+    @PostMapping(path = "/validation", produces = "application/json")
+    public ResponseEntity<AcknowledgeResponse> mpesaValidation(@RequestBody MpesaValidationResponse mpesaValidationResponse) {
+        return ResponseEntity.ok(acknowledgeResponse);
+    }
+
+    @PostMapping(path = "/simulate-c2b", produces = "application/json")
+    public ResponseEntity<SimulateTransactionResponse> simulateB2CTransaction(@RequestBody SimulateTransactionRequest simulateTransactionRequest) {
+        return ResponseEntity.ok(darajaApi.simulateC2BTransaction(simulateTransactionRequest));
     }
 }
