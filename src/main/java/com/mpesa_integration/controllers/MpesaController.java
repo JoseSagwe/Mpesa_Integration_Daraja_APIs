61e82972524cd78527145d4577a43042aa1ca93f
Joseph Sagwe <josephsagwe421@gmail.com>
2024-02-24 20:13:32 +0300

    Update MpesaController.java

--- src/main/java/com/mpesa_integration/controllers/MpesaController.java
+++ src/main/java/com/mpesa_integration/controllers/MpesaController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 @RequestMapping("mpesa")
 public class MpesaController {
@@ -14,6 +15,7 @@ public class MpesaController {
     public MpesaController(DarajaApi darajaApi) {
         this.darajaApi = darajaApi;
     }
+    
     @GetMapping(path = "/token", produces = "application/json")
     public ResponseEntity<AccessTokenResponse> getAccessToken() {
         return ResponseEntity.ok(darajaApi.getAccessToken());
